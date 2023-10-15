01962171284dabe1aaedafc62bce1266da69683c
mamangbili <legendaryma30@gmail.com>
2023-10-15 20:32:25 +0700

    penyesuain struktur data dengan MockApi free

--- scrappingData/scrapping.js
+++ scrappingData/scrapping.js
@@ -22,7 +22,8 @@ async function getFoodLink() {
 
 async function scrapGiziRingkas(cheerioRootHTML) {
     const $ = cheerioRootHTML
-    const giziRingkas = {}
+    const namaMakanan = $("h1").text()
+    const giziRingkas = { namaMakanan }
     $('td.fact').each((i, each) => {
 
         const title = $(each).find('.factTitle').text()
@@ -38,30 +39,30 @@ function scrapGiziLengkap(cheerioRootHTML) {
     const kanan = $(kotak).find(".nutrient.right.tRight")
     const kiri = $(kotak).find(".nutrient.left")
     const giziLengkap = {}
-    $(kiri).each((i,each) => {
+    $(kiri).each((i, each) => {
 
-        let property = $(each).text() 
+        let property = $(each).text()
         property = property === '' ? 'EnergiInKal' : property
         const value = $(kanan).eq(i).text()
 
-        giziLengkap[property] = value 
+        giziLengkap[property] = value
     })
     return giziLengkap
 }
 
-async function scrapLengkapDanRingkas(subLink){
+async function scrapLengkapDanRingkas(subLink) {
     const fatsecret = "https://www.fatsecret.co.id"
-        
-    const fullLink = fatsecret+subLink
+
+    const fullLink = fatsecret + subLink
     const makananHTML = await getCheerioRoot(fullLink)
     const dataRingkas = await scrapGiziRingkas(makananHTML)
     const dataLengkap = await scrapGiziLengkap(makananHTML)
-    return {dataRingkas, dataLengkap}
+    return { ...dataRingkas, ...dataLengkap }
 }
 
 const request = []
 const links = await getFoodLink()
-for(const link of links){
+for (const link of links) {
     request.push(scrapLengkapDanRingkas(link))
 }
 
@@ -70,14 +71,14 @@ const resolveRequest = await Promise.all(request)
 console.log(resolveRequest.length)
 const jsonString = JSON.stringify(resolveRequest, null, 2);
 
-const filePath = 'hasilScrapMakanan.json';
+const filePath = 'scrappingData/hasilScrapMakanan.json';
 
 fs.writeFile(filePath, jsonString, 'utf8', (err) => {
-  if (err) {
-    console.error('Error saving the JSON file:', err);
-  } else {
-    console.log('JSON file saved successfully.');
-  }
+    if (err) {
+        console.error('Error saving the JSON file:', err);
+    } else {
+        console.log('JSON file saved successfully.');
+    }
 });
 
 
